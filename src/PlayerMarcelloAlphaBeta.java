29150fb4f3be9e974c05f684b0463f3eb03a4161
Marcello De Bernardi <marcello1234@live.co.uk>
2018-02-05 11:21:23 +0000

    Separate AB and ML implementations and add docs

---
+++ src/PlayerMarcelloAlphaBeta.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+/**
+ * @author Marcello De Bernardi 05/02/2018.
+ */
+public class PlayerMarcelloAlphaBeta extends GomokuPlayer {
+    @Override
+    public Move chooseMove(Color[][] colors, Color color) {
+        return null;
+    }
+}
--- src/PlayerMarcelloML.java
+++ src/PlayerMarcelloML.java
@@ -3,7 +3,7 @@ import java.awt.*;
 /**
  * @author Marcello De Bernardi 05/02/2018.
  */
-public class Player150382405 extends GomokuPlayer {
+public class PlayerMarcelloML extends GomokuPlayer {
     @Override
     public Move chooseMove(Color[][] colors, Color color) {
         return null;
